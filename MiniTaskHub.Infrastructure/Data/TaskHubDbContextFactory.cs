50c97ebfc153a53e56f280c736f9142cba0ddd42
Novav20 <illus.kun@gmail.com>
2025-05-08 09:45:17 -0500

    db context factory

---
+++ MiniTaskHub.Infrastructure/Data/TaskHubDbContextFactory.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MiniTaskHub.Infrastructure.Data
+{
+    public class TaskHubDbContextFactory : IDesignTimeDbContextFactory<TaskHubDbContext>
+    {
+        public TaskHubDbContext CreateDbContext(string[] args)
+        {
+            // Go up to the API project directory
+            var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "../MiniTaskHub.Api"));
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<TaskHubDbContext>();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new TaskHubDbContext(optionsBuilder.Options);
+        }
+    }
+}
