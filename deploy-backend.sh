b71c91eb84b52dadf2a06ab2f7a13b18a2ab3764
Novav20 <illus.kun@gmail.com>
2025-05-10 10:33:23 -0500

    fix: update navigationFallback and responseOverrides in staticwebapp.config.json

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -5,3 +5,5 @@ cd ./publish
 zip -r ../publish.zip .
 cd ..
 az webapp deploy --resource-group StudentResourceGroup --name minitaskhub-api --src-path ./publish.zip --type zip
+rm -rf ./publish
+rm ./publish.zip
